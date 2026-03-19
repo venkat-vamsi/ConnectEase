55f2cbd3299f523ee934f17860e1bb988a3b1ac2
venkat-vamsi <vamsigodavarthi2004@gmail.com>
2026-03-19 13:56:20 +0530

    backend folder

---
+++ connectease_backend/src/main/java/com/cts/connectease/ConnecteaseApplication.java
@@ -0,0 +1,13 @@
+package com.cts.connectease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnecteaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnecteaseApplication.class, args);
+	}
+
+}
---
+++ connectease_backend/src/test/java/com/cts/connectease/ConnecteaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.connectease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecteaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
